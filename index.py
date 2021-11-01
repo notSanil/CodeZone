14b7185a743e98e74f8fa8e9f64fbce831376ea5
notSanil <44319106+notSanil@users.noreply.github.com>
2021-11-01 21:08:03 +0530

    added the urls to the backend

--- index.py
+++ index.py
@@ -9,6 +9,30 @@ app.debug = True
 def index():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/compete")
+def compete():
+    return render_template('compete.html')
+
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template('leaderboard.html')
+
+@app.route("/practice")
+def practice():
+    return render_template('practice.html')
+
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
+
+@app.route("/trending")
+def trending():
+    return render_template('trending.html')
+
 
 if __name__ == '__main__':
     app.run()
