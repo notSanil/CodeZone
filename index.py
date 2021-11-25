c7243e44ccf8144361dd6a6f1c0e81d0071b3d08
shiraz-30 <78081835+shiraz-30@users.noreply.github.com>
2021-11-25 15:22:57 +0530

    practice pg

--- index.py
+++ index.py
@@ -89,10 +89,14 @@ def practice():
     filename = 'data/sorted_problems.csv'
     data = pandas.read_csv(filename, header=0)
     data.columns = data.columns.str.strip()
-    easytbl = list(data[data['Rating']<=800].sample(20).values)
-    midtbl = list(data[(data['Rating']<=1600) & (data['Rating']>800)].sample(20).values)
-    hardtbl = list(data[(data['Rating']<=2500) & (data['Rating']>1600)].sample(20).values)
-    insanetbl = list(data[data['Rating']>2500].sample(20).values)
+    easytbl = list(data[data['Rating']<=800].sample(5).values)
+    midtbl = list(data[(data['Rating']<=1600) & (data['Rating']>800)].sample(5).values)
+    hardtbl = list(data[(data['Rating']<=2500) & (data['Rating']>1600)].sample(5).values)
+    insanetbl = list(data[data['Rating']>2500].sample(5).values)
+    print("Easy:" , easytbl)
+    print("Medium:" , midtbl)
+    print("Hard:" , hardtbl)
+    print("Insane:" , insanetbl)
     return render_template('practice.html', easytbl=easytbl, midtbl=midtbl, hardtbl=hardtbl, insanetbl=insanetbl)
 
 @app.route("/profilepage")
---
+++ static/js/practice.js
@@ -0,0 +1,27 @@
+function myFunction1(){
+    document.getElementById("easytbl").style.display="block";
+    document.getElementById("midtbl").style.display="none";
+    document.getElementById("hardtbl").style.display="none";
+    document.getElementById("insanetbl").style.display="none";
+}
+
+function myFunction2(){
+    document.getElementById("easytbl").style.display="none";
+    document.getElementById("midtbl").style.display="block";
+    document.getElementById("hardtbl").style.display="none";
+    document.getElementById("insanetbl").style.display="none";
+}
+
+function myFunction3(){
+    document.getElementById("easytbl").style.display="none";
+    document.getElementById("midtbl").style.display="none";
+    document.getElementById("hardtbl").style.display="block";
+    document.getElementById("insanetbl").style.display="none";
+}
+
+function myFunction4(){
+    document.getElementById("easytbl").style.display="none";
+    document.getElementById("midtbl").style.display="none";
+    document.getElementById("hardtbl").style.display="none";
+    document.getElementById("insanetbl").style.display="block";
+}
