da9c699c3f96c14dd68e3f3df3431d1f9f31564e
notSanil <44319106+notSanil@users.noreply.github.com>
2021-11-26 22:15:59 +0530

    graphs now work, sort of

--- Graph/graph_generation.py
+++ Graph/graph_generation.py
@@ -3,76 +3,104 @@ import matplotlib.pyplot as plt
 import mpld3
 import pandas as pd
 import numpy as np
-def generate_html_for_graph_questions(ques, date):
-    
+import ctypes
+
+user32 = ctypes.windll.user32
+screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+plt.switch_backend('agg')
+
+
+def select_even(seq, num):
+    length = float(len(seq))
+    res = []
+    for i in range(num):
+        res.append(seq[int(np.ceil(i * length / num))])
+    return res
+
+
+def questions_graph(ques, date):
+    """
+    Generates graph for number of questions solved per day, takes as input a list of questions solved each
+    day
+    """
     dates = [date]
-    while (len(dates) != len(ques)):
-        dates.append(date + datetime.timedelta(days= 1))
-    
+    print(len(dates), len(ques))
+    while not (len(dates) >= len(ques)):
+        
+        dates.append(date + datetime.timedelta(days= len(dates)))
+    print(dates)
     a = plt.stackplot(dates, ques, colors=['#001333'])
+
+    ticks = select_even(dates, min(5, len(dates)))
     
-    plt.xlabel("Date")
-    plt.ylabel("Number of questions solved")
+    plt.xlabel("Date", color='#F1884D')
+    plt.xticks(ticks)
+    print(ticks)
     plt.title("Questions solved per day")
     plt.grid(True, color='#EFF3FF')# set graph grid color and control its pres
     ax = plt.axes()
     ax.set_facecolor('white') #set graph bg-color
     b = a[0].figure
-    # b.set_figwidth(10)
-    # b.set_figheight(10)
+    b.set_figwidth((screensize[0] * 0.3 * 1)/100.0)
+    b.set_figheight((screensize[1] * 0.54 * 0.5)/100.0)
     
     code = mpld3.fig_to_html(b)
     
     return code
 
 
-def generate_html_for_graph_rank(rank, date):
-    
+def rank_graph(rank, date):
+    """
+    Generates the graph for the rank change per day, takes as input the rank list
+    """
     dates_ = [date]
-    while (len(dates_) != len(rank)):
-        dates_.append(date + datetime.timedelta(days= 1))
-    
+    while not (len(dates_) >= len(rank)):
+        dates_.append(date + datetime.timedelta(days= len(dates_)))    
     
     a = plt.plot(dates_, rank, color = '#F1884D', linewidth = 5)
     plt.xlabel("Date")
-    plt.ylabel("Rank in leaderboard")
-    
+    plt.title("Rank Change")
+    ticks = select_even(dates_, min(5, len(dates_)))
+    plt.xticks(ticks)
     plt.grid(True, color='#EFF3FF')
+    
     ax = plt.axes()
     ax.set_facecolor('white')
     b = a[0].figure
-    # b.set_figwidth(10)
-    # b.set_figheight(10)
+    b.set_figwidth((screensize[0] * 0.3 * 1)/100.0)
+    b.set_figheight((screensize[1] * 0.54 * 0.5)/100.0)
     
     code = mpld3.fig_to_html(b)
+    print(code)
     
     return code
 
 
-def generate_html_for_graph_xp(xp, date):
-    
+def xp_graph(xp, date):
+    """
+    Generates the graph for the xp change
+    """
     dates_ = [date]
-    while (len(dates_) != len(xp)):
-        dates_.append(dates_[-1] + datetime.timedelta(days = 1))
+    while not (len(dates_) >= len(xp)):
+        dates_.append(date + datetime.timedelta(days = len(dates_)))
     dates_ = np.array(dates_)
     df = pd.DataFrame(xp)
     fig, ax = plt.subplots()
+    ticks = select_even(dates_, min(5, len(dates_)))
+    plt.xticks(ticks)
+    plt.title("XP change per day")
     ax.grid(True, alpha=0.3, color='#EFF3FF')
     for key, val in df.iteritems():
         l, = ax.plot(dates_, val.values, color='#001333', linewidth = 5)
         # ax.legend()
         ax.set_xlabel("Date")
-        ax.set_ylabel("XP")
         ax.fill_between(dates_,
                         val.values * .5, val.values * 1.5,
                         color='#F1884D', alpha=.4)
         ax.set_facecolor('white')
     # b = a[0].figure
-    # fig.set_figwidth(10)
-    # fig.set_figheight(10)
+    fig.set_figwidth((screensize[0] * 0.3 * 1)/100.0)
+    fig.set_figheight((screensize[1] * 0.54 * 0.5)/100.0)
     
     code = mpld3.fig_to_html(fig)
-    # print(code)
     return code
-# generate_html_for_graph_questions([5, 2, 4, 5, 2, 9, 0, 1], datetime.date.today())
-
--- db_handler.py
+++ db_handler.py
@@ -28,8 +28,9 @@ class db:
             conn = self.get_conn()
             cursor = conn.cursor()
             cursor.execute("""CREATE TABLE IF NOT EXISTS {}(id text PRIMARY KEY, 
-            name TEXT NOT NULL, email text, handle text, xp int DEFAULT 100, 
-            solved json, recommended json DEFAULT '[]'::json)""".format(self.TNAME))
+            name TEXT NOT NULL, email text, handle text, signin date, xp int DEFAULT 100, 
+            solved json, recommended json DEFAULT '[]'::json, 
+            qpd json DEFAULT '[]'::json, xp_pd json DEFAULT '[]'::json, rpd json DEFAULT '[]'::json)""".format(self.TNAME))
             cursor.execute("commit")
             self.database = cursor
         
