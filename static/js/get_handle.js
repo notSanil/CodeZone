201973ffc2a2a07234d89c0dd54a7e95ead534fc
shiraz-30 <78081835+shiraz-30@users.noreply.github.com>
2021-11-29 18:28:46 +0530

    get handle updated

---
+++ static/js/get_handle.js
@@ -0,0 +1,26 @@
+
+function ModeFunction() {
+    var x = localStorage.getItem('modes');
+    if (x == 'Dark') {
+        document.getElementById('mode').click();
+    }
+}
+
+function toggle_function() {
+    var r = document.querySelector(':root')
+    if (document.getElementById('mode').innerText == 'Light') {
+        
+        document.getElementById('mode').innerText = 'Dark';
+
+        r.style.setProperty("--navyblue", "#eff3ff");
+        r.style.setProperty("--lightblue", "#001333");
+        r.style.setProperty("--grey", "#45546e");
+    } 
+    else {
+        document.getElementById('mode').innerText = "Light";
+
+        r.style.setProperty("--lightblue", "#eff3ff");
+        r.style.setProperty("--navyblue", "#001333");
+        r.style.setProperty("--grey", "#c3c3c3");
+    }
+}
