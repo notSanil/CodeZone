8c445a72b79c192432fb4b35cde293e94edc50cb
Uttam Mittal <78130443+uttammittal02@users.noreply.github.com>
2022-01-23 15:17:34 +0530

    Update problem_lens.py

--- model/problem_lens.py
+++ model/problem_lens.py
@@ -1,6 +1,4 @@
-import os
 import csv
-import sys
 import re
 
 from surprise import Dataset
@@ -9,27 +7,23 @@ from surprise import Reader
 from collections import defaultdict
 import pandas as pd
 
-from model.gen_data import convert_into_df
 
 
+'''
+In case of item based collaborative filtering, this file is only required to train the model. For getting predictions from the model, we just need 2 files obtained during the training period
+'''
 
 class ProblemLens:
 
-    ratingsPath = 'data\\user_data_1.csv'
-    problemsPath = 'data/problem_set.csv'
+    ratingsPath = './data/user_data_1.csv'
+    problemsPath = './data/problem_set.csv'
 
-    def loadProblemLens(self, username, solved):
-
-        # Look for files relative to the directory we are running from
-        #os.chdir(os.path.dirname(sys.argv[0]))
+    def loadProblemLens(self):
 
         score_dataset = 0
         
         df = pd.read_csv(self.ratingsPath).iloc[:, 0:3]
-        userData = convert_into_df(username, solved)
-        userDf = pd.DataFrame(userData).iloc[:, 0:3]
-        df = df.append(userDf, ignore_index=True)
-
+        
         reader = Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
         score_dataset = Dataset.load_from_df(df= df, reader=reader)
         
@@ -39,44 +33,44 @@ class ProblemLens:
 
     def gettags(self):
         tags = defaultdict(list)
-        genreIDs = {}
-        maxGenreID = 0
+        tagIDs = {}
+        maxtagID = 0
         with open(self.problemsPath, newline='', encoding='ISO-8859-1') as csvfile:
-            movieReader = csv.reader(csvfile)
-            next(movieReader)  #Skip header line
-            for row in movieReader:
-                movieID = int(row[0])
-                genreList = row[2]
-                genreIDList = []
-                for genre in genreList:
-                    if genre in genreIDs:
-                        genreID = genreIDs[genre]
+            problemReader = csv.reader(csvfile)
+            next(problemReader)  #Skip header line
+            for row in problemReader:
+                problemID = int(row[0])
+                tagList = row[2]
+                tagIDList = []
+                for tag in tagList:
+                    if tag in tagIDs:
+                        tagID = tagIDs[tag]
                     else:
-                        genreID = maxGenreID
-                        genreIDs[genre] = genreID
-                        maxGenreID += 1
-                    genreIDList.append(genreID)
-                tags[movieID] = genreIDList
-        for (movieID, genreIDList) in tags.items():
-            bitfield = [0] * maxGenreID
-            for genreID in genreIDList:
-                bitfield[genreID] = 1
-            tags[movieID] = bitfield            
+                        tagID = maxtagID
+                        tagIDs[tag] = tagID
+                        maxtagID += 1
+                    tagIDList.append(tagID)
+                tags[problemID] = tagIDList
+        for (problemID, tagIDList) in tags.items():
+            bitfield = [0] * maxtagID
+            for tagID in tagIDList:
+                bitfield[tagID] = 1
+            tags[problemID] = bitfield            
         
         return tags
 
     def get_problem_ratings(self):
         p = re.compile(r"(?:\((\d{4})\))?\s*$")
-        years = defaultdict(int)
+        ratings = defaultdict(int)
         with open(self.problemsPath, newline='', encoding='ISO-8859-1') as csvfile:
-            movieReader = csv.reader(csvfile)
-            next(movieReader)
-            for row in movieReader:
-                movieID = int(row[0])
-                year = row[1]
-                if year:
-                    years[movieID] = int(year)
-        return years
+            problemReader = csv.reader(csvfile)
+            next(problemReader)
+            for row in problemReader:
+                problemID = int(row[0])
+                rating = row[1]
+                if rating:
+                    ratings[problemID] = int(rating)
+        return ratings
 
 if __name__ == '__main__':
     ProblemLens().loadProblemLens('notSanil')
